d00e2f9a8da37f0b48ebff05b8e541217af9f1a6
Himaster <himaster@inbox.ru>
2015-07-09 16:28:36 +0300

    html begin

--- index.php
+++ index.php
@@ -4,6 +4,21 @@
 	$dbuser = 'confogen';
 	$dbpassword = 'Bie0gaen';
 	$dbname = 'confogen';
-	
+
 	#DB
-	
+	$link = mysql_connect('localhost', $dbuser, $dbpassword);
+	if (!$link) {
+    	die('Ошибка соединения: ' . mysql_error());
+	}
+
+	#HTML
+	?>
+	<HTML>
+		<HEAD>
+			<TITLE>ConfoGen</TITLE>
+		</HEAD>
+		<BODY>
+			<TABLE align='center'>
+				<TR>
+					<TD>No</TD>
+					<TD>Name</TD>
