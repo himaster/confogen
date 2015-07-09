c7cef271ba053692eb061a63f453a497aac72c58
Himaster <himaster@inbox.ru>
2015-07-09 18:10:56 +0300

    mysql php fix

--- index.php
+++ index.php
@@ -1,20 +1,7 @@
 <?php
 
 	#Variables
-	$dbuser = 'confogen';
-	$dbpassword = 'Bie0gaen';
-	$dbname = 'confogen';
-
-	#DB
-	$link = mysql_connect('localhost', $dbuser, $dbpassword);
-	if (!$link) {
-    	die('Ошибка соединения: ' . mysql_error());
-	}
 	
-	$db_selected = mysql_select_db($dbname, $link);
-	if (!$db_selected) {
-    	die ('Не удалось выбрать базу foo: ' . mysql_error());
-	}
 
 	include "header.html";
 	
