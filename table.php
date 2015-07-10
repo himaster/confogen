8f25511a75092001578b7daf65cae59d6bbd1e91
Himaster <himaster@inbox.ru>
2015-07-10 10:57:58 +0300

    makeup

--- table.php
+++ table.php
@@ -31,8 +31,8 @@
     $result = mysql_query($sql, $link)  or die(mysql_error());
 
     echo "<TABLE align='center'>";
-	echo "<TR><TD width='10'>#</TD>";
-	echo "<TD width='40'>Name</TD>";
+	echo "<TR><TD width='20'>#</TD>";
+	echo "<TD width='70'>Name</TD>";
 	echo "<TD>IP</TD>";
 	echo "<TD>HTTP</TD>";
 	echo "<TD>HTTPS</TD>";
