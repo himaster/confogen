4371485a6862ea59baaf162426ca5c53eeca8d13
Himaster <himaster@inbox.ru>
2015-07-10 14:27:39 +0300

    adding table_view.php

--- table.php
+++ table.php
@@ -38,35 +38,5 @@
 	$sql = "SELECT * FROM `domains`;";
     $result = mysql_query($sql, $link)  or die(mysql_error());
 
-    echo "<TABLE align='center'>";
-	echo "<TR><TD width='20'>#</TD>";
-	echo "<TD width='140'>Name</TD>";
-	echo "<TD width='165'>IP</TD>";
-	echo "<TD width='40'>HTTP</TD>";
-	echo "<TD width='50'>HTTPS</TD>";
-	echo "<TD width='45'>TEST</TD>";
-	echo "<TD width='50'>Mobile</TD>";
-	echo "<TD width='60'>M.test</TD>";
-	echo "<TD width='60'><FONT size='2'>Remove</FONT></TD></TR>";
-
-	while ($row = mysql_fetch_assoc($result)) {
-		echo "<TR><TD width='20'>".$row['id']."</TD>";
-		echo "<TD width='140'>".$row['name']."</TD>";
-		echo "<TD width='165'>".$row['ip']."</TD>";
-		echo "<TD width='40'><input type='checkbox' ";
-		if ($row['http'] == 1) echo "checked";
-		echo "></TD>";
-		echo "<TD width='50'><input type='checkbox' ";
-		if ($row['https'] == 1) echo "checked";
-		echo "></TD>";
-		echo "<TD width='45'><input type='checkbox' ";
-		if ($row['test'] == 1) echo "checked";
-		echo "></TD>";
-		echo "<TD width='50'><input type='checkbox' ";
-		if ($row['m'] == 1) echo "checked";
-		echo "></TD>";
-		echo "<TD width='60'><input type='checkbox' ";
-		if ($row['mtest'] == 1) echo "checked";
-		echo "></TD>";
-		echo "<TD width='60'><A href='javascript:rem_el(".$row['id'].")'>x</A></TD></TR>";
-	}
+    include 'table_view.php';
+    
---
+++ table_view.php
@@ -0,0 +1,22 @@
+<TABLE align='center'>
+<TR><TD width='20'>#</TD>
+<TD width='140'>Name</TD>
+<TD width='165'>IP</TD>
+<TD width='40'>HTTP</TD>
+<TD width='50'>HTTPS</TD>
+<TD width='45'>TEST</TD>
+<TD width='50'>Mobile</TD>
+<TD width='60'>M.test</TD>
+<TD width='60'><FONT size='2'>Remove</FONT></TD></TR>
+
+<?php while ($row = mysql_fetch_assoc($result)) { ?>
+		<TR><TD width='20'><?php $row['id'] ?></TD>
+		<TD width='140'><?php $row['name'] ?></TD>
+		<TD width='165'><?php $row['ip'] ?></TD>
+		<TD width='40'><input type='checkbox' <?php if ($row['http'] == 1) echo "checked"; ?> ></TD>;
+		<TD width='50'><input type='checkbox' <?php if ($row['https'] == 1) echo "checked"; ?> ></TD>;
+		<TD width='45'><input type='checkbox' <?php if ($row['test'] == 1) echo "checked"; ?> ></TD>;
+		<TD width='50'><input type='checkbox' <?php if ($row['m'] == 1) echo "checked"; ?> ></TD>;
+		<TD width='60'><input type='checkbox' <?php if ($row['mtest'] == 1) echo "checked"; ?> ></TD>;
+		<TD width='60'><A href='javascript:rem_el(<?php $row['id'] ?>)'>x</A></TD></TR>;
+<?php } ?>
