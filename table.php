7f42f40145543f6b0dc6e61c2dc0fd4f92c197fa
Himaster <himaster@inbox.ru>
2015-07-10 12:03:12 +0300

    element deletion

--- table.php
+++ table.php
@@ -14,8 +14,14 @@
     	die ('Не удалось выбрать базу foo: ' . mysql_error());
 	}
 
+	#element delete
+	if ($_GET['remove']){
+		$id = $_GET['id'];
+		$sql = "DELETE FROM `domains` WHERE id=$id;";
+	}
+
 	#Form submit
-	if (!empty($_GET['name']) && !empty($_GET['ip'])) {
+	else if (!empty($_GET['name']) && !empty($_GET['ip'])) {
 		$name = $_GET['name'];
 		$ip = $_GET['ip'];
 		$http = $_GET['http'];
@@ -61,5 +67,6 @@
 		echo "<TD><input type='checkbox' ";
 		if ($row['mtest'] == 1) echo "checked";
 		echo "></TD>";
+		echo "<TD><A href=\"javascript:rem_el($row['id'])\">x</A></TD>";
 		echo "</TR>";
 	}
