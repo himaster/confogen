ff7221b8c0d9c16163bbc4b6705cd47eafa1c351
Himaster <himaster@inbox.ru>
2015-07-10 18:19:18 +0300

    generator fix

--- generator.php
+++ generator.php
@@ -43,11 +43,14 @@
     		fwrite($handle, "	ssl				on;\n");
 			fwrite($handle, "	ssl_certificate			".$certfile.".crt;\n");
 			fwrite($handle, "	ssl_certificate_key		".$certfile.".key;\n\n");
-	    	fwrite($handle, "	rewrite			^/(.*)$  https://www.".$name."/$1  permanent;\n}\n\n");
+	    	fwrite($handle, "	rewrite	^/(.*)$  https://www.".$name."/$1  permanent;\n}\n\n");
 	    	fwrite($handle, "## HTTPS\n");
 	    	fwrite($handle, "server {\n");
     		fwrite($handle, "	listen ".$ip.":443 ssl;\n");
     		fwrite($handle, "	server_name www.".$name.";\n\n");
+    		fwrite($handle, "	ssl				on;\n");
+			fwrite($handle, "	ssl_certificate			".$certfile.".crt;\n");
+			fwrite($handle, "	ssl_certificate_key		".$certfile.".key;\n\n");
 	    	fwrite($handle, "	location / {\n");
 	    	fwrite($handle, "		proxy_set_header	Host			\$http_host;\n");
 	    	fwrite($handle, "		proxy_set_header	X-Real-IP		\$remote_addr;\n");
