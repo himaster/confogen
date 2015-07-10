21f73db2d9bd7ae59a3e50a589fe18c44fb98de2
Himaster <himaster@inbox.ru>
2015-07-10 17:23:50 +0300

    generate.php fix

--- generate.php
+++ generate.php
@@ -1,2 +1,19 @@
 <?php
 	include 'db.php';
+	$workdir = '/etc/nginx/fpm-conf.d/balancer/';
+	$sql = "SELECT * FROM `domains`;";
+    $result = mysql_query($sql, $link)  or die(mysql_error());
+    while ($row = mysql_fetch_assoc($result)) {
+    	$id = $row['id'];
+    	$name = $row['name'];
+    	$ip = $row['ip'];
+    	$http = $row['http'];
+    	$https = $row['https'];
+    	$test = $row['test'];
+    	$m = $row['m'];
+    	$mtest = $row['mtest'];
+    	$file = $workdir$name'.conf';
+    	echo($file);
+    	#file_put_contents($file, $current);
+    }
+?>
