1d64516320258207fe3b7be68721112d1098e153
Himaster <himaster@inbox.ru>
2015-07-15 18:54:48 +0300

    editing implementation

--- table.php
+++ table.php
@@ -6,8 +6,19 @@
 		$id = $_GET['id'];
 		$sql = "DELETE FROM `domains` WHERE id=$id;";
 		$result = mysql_query($sql, $link)  or die(mysql_error());
+	} elseif ($_GET['act'] == "edit") {
+		$id = $_GET['id'];
+		$name = $_GET['name'];
+		$ip = $_GET['ip'];
+		$http = $_GET['http'];
+		$https = $_GET['https'];
+		$cert = $_GET['cert'];
+		$test = $_GET['test'];
+		$m = $_GET['m'];
+		$mtest = $_GET['mtest'];
+		$sql = "UPDATE `domains` SET name = '$name', ip = '$ip', http = '$http', https = '$https', cert = '$cert', test = '$test', m = '$m', mtest = '$mtest' WHERE id = '$id'";
+		$result = mysql_query($sql, $link)  or die(mysql_error());
 	}
-
 	#Form submit
 	else if (!empty($_GET['name']) && !empty($_GET['ip'])) {
 		$name = $_GET['name'];
