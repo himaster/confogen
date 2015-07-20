52b991efa9e5d3f520fa2a47866705a19621af99
Himaster <himaster@inbox.ru>
2015-07-20 14:40:57 +0300

    debug

--- generator.php
+++ generator.php
@@ -2,6 +2,7 @@
 	include 'db.php';
 	$workdir = '/etc/nginx/fpm-conf.d/balancer/';
 	$certdir = '/etc/nginx/certs/';
+	die("------------");
 	#$files = glob($workdir.'*'); // get all file names
 	#foreach($files as $file){ // iterate files
   	#	if(is_file($file)) unlink($file); // delete file
