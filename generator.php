d8dae8176889d496654caf1da5d30ce68cc95ba9
Himaster <himaster@inbox.ru>
2015-08-25 12:34:43 +0300

    test

--- generator.php
+++ generator.php
@@ -17,7 +17,6 @@
 	else {
 		$sql = "SELECT * FROM `domains` WHERE id=".$_GET['id'].";";
 	}
-	echo("Test");
     $result = mysql_query($sql, $link) or die(mysql_error());
     while ($row = mysql_fetch_assoc($result)) {
     	$id = $row['id'];
