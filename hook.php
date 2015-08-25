2adfd92b1a545d6a4354eca40f8421ef9d3c9c47
Himaster <himaster@inbox.ru>
2015-08-25 12:05:14 +0300

    hook test fix

--- hook.php
+++ hook.php
@@ -1,5 +1,5 @@
 <?php
     putenv('PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/var/www/confogen/');
-    exec('cd /var/www/confogen/ && /usr/bin/git pull https://github.com/himaster/confogen.git master 2>&1', $output);
+    exec('cd /var/www/confogen/ && sudo git pull https://github.com/himaster/confogen.git master 2>&1', $output);
     print_r($output);
 ?>
