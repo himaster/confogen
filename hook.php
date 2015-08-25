15f241f40cb88359b21a2cb5083c77364ee905d1
Himaster <himaster@inbox.ru>
2015-08-25 11:48:00 +0300

    hook fix

--- hook.php
+++ hook.php
@@ -1,4 +1,4 @@
 <?php
-    putenv('PATH='. getenv('PATH') .':/home/developer/www/confogen/');
-    exec('cd /home/developer/www/confogen/ && git pull https://github.com/himaster/confogen.git master 2>&1', $output);
+    putenv('PATH='. getenv('PATH') .':/var/www/confogen/');
+    exec('cd /var/www/confogen/ && git pull https://github.com/himaster/confogen.git master 2>&1', $output);
 ?>
