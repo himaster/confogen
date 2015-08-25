58dd6bcdce09e33b16411f9c42109ebab1c00769
Himaster <himaster@inbox.ru>
2015-08-25 12:01:15 +0300

    test

--- hook.php
+++ hook.php
@@ -1,6 +1,6 @@
 <?php
-	print_r(getenv('PATH'));
+	print_r("PATH= ".getenv('PATH'));
     putenv('PATH='. getenv('PATH') .':/var/www/confogen/');
-    exec('cd /var/www/confogen/ && sudo /usr/bin/git pull https://github.com/himaster/confogen.git master 2>&1', $output);
+    exec('cd /var/www/confogen/ && /usr/bin/git pull https://github.com/himaster/confogen.git master 2>&1', $output);
     print_r($output);
 ?>
