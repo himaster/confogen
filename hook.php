80fd0343cb1c4d60983cb20c39640c4198767d9e
Himaster <himaster@inbox.ru>
2015-08-25 12:02:51 +0300

    Hook path fix

--- hook.php
+++ hook.php
@@ -1,6 +1,5 @@
 <?php
-	print_r("PATH= ".getenv('PATH'));
-    putenv('PATH='. getenv('PATH') .':/var/www/confogen/');
+    putenv('PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/var/www/confogen/');
     exec('cd /var/www/confogen/ && /usr/bin/git pull https://github.com/himaster/confogen.git master 2>&1', $output);
     print_r($output);
 ?>
