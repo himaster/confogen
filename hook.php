049e7631cf139c6aeb658160683ec943247adbbc
Himaster <himaster@inbox.ru>
2015-08-25 11:56:10 +0300

    hook output enabled

--- hook.php
+++ hook.php
@@ -1,4 +1,5 @@
 <?php
     putenv('PATH='. getenv('PATH') .':/var/www/confogen/');
     exec('cd /var/www/confogen/ && git pull https://github.com/himaster/confogen.git master 2>&1', $output);
+    print_r($output);
 ?>
