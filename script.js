dcefb4b0e6d37752238def3cd4cdb61e64cb2f2a
Himaster <himaster@inbox.ru>
2015-09-29 16:54:10 +0300

    test

--- script.js
+++ script.js
@@ -167,10 +167,10 @@ $(function() {
         }
     })
 
-    // $(document).on('keydown', '#maintable tr', function(e) {
-    //     var curr = $(this);
-    //     console.log(curr.index(), e);
-        
-    // })
+    $(document).on('keydown', '#maintable tr', function() {
+        var curr = $(this);
+        console.log(curr.index());
+
+    })
 })
 -->
