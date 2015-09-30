1cafc5848be3ae410184a1078ec416078e11b23e
Himaster <himaster@inbox.ru>
2015-09-30 12:31:46 +0300

    iconv test

--- table.php
+++ table.php
@@ -53,7 +53,7 @@
 		else
 			$mtest = 0;
 		#$comment = $_GET['comment'];
-		$comment = iconv('windows-1251', 'UTF-8', $_GET['comment']);
+		$comment = iconv('UTF-8', 'windows-1251', $_GET['comment']);
 		$sql = "UPDATE `domains` SET name = '$name', ip = '$ip', http = '$http', https = '$https', cert = '$cert', test = '$test', blog = '$blog', blogname = '$blogname', m = '$m', mtest = '$mtest', comment = '$comment' WHERE id = '$id'";
 		$result = mysql_query($sql, $link)  or die(mysql_error());
 	}
