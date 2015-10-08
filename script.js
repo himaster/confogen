d45fc4af3c6b5aecbb6e00aeef15ad6b831f4460
Himaster <himaster@inbox.ru>
2015-10-08 11:55:13 +0300

    cookies test

--- script.js
+++ script.js
@@ -156,6 +156,34 @@ function textSelector(s){
     tr.select();
 }
 
+function setCookie (name, value, expires, path, domain, secure) {
+      document.cookie = name + "=" + escape(value) +
+        ((expires) ? "; expires=" + expires : "") +
+        ((path) ? "; path=" + path : "") +
+        ((domain) ? "; domain=" + domain : "") +
+        ((secure) ? "; secure" : "");
+}
+
+function getCookie(name) {
+    var cookie = " " + document.cookie;
+    var search = " " + name + "=";
+    var setStr = null;
+    var offset = 0;
+    var end = 0;
+    if (cookie.length > 0) {
+        offset = cookie.indexOf(search);
+        if (offset != -1) {
+            offset += search.length;
+            end = cookie.indexOf(";", offset)
+            if (end == -1) {
+                end = cookie.length;
+            }
+            setStr = unescape(cookie.substring(offset, end));
+        }
+    }
+    return(setStr);
+}
+
 $(function() {
     $(document).on('mousemove', 'textarea', function(e) {
         var a = $(this).offset().top + $(this).outerHeight() - 16,  //  top border of bottom-right-corner-box area
@@ -193,6 +221,7 @@ $(function() {
 
     $(document).on('keydown', '#maintable tr', function(e) {
         var curr = $(this);
+        console.log(curr.index());
         if(e.keyCode == 40 ){
             e.preventDefault();
             var next = $($("#maintable tr").get(curr.index()+1));
