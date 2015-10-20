47a0cfa20c5ed1e2b5a325f1e830cb1436c5bbc8
Himaster <himaster@inbox.ru>
2015-10-20 15:09:25 +0300

    css fix

--- table_view.php
+++ table_view.php
@@ -14,7 +14,7 @@
 				<TD><b><FONT size="1">M.test</FONT></b></TD>
 				<TD><b>Comment</b></TD>
 				<TD><b><FONT size='1'>Ed.</FONT></b></TD>
-				<TD><b><FONT size='1'>Rem</FONT></b></TD>
+				<TD><b><FONT size='1'>Re.</FONT></b></TD>
 			</TR>
 		</TABLE>
 		<TABLE class="maintable" id="newtable">
