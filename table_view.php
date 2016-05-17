acd3f0a4f64dcaa91cee4859fc61f3a73438fe15
Himaster <himaster@inbox.ru>
2016-05-17 12:50:21 +0300

    bold added to column tabindex

--- table_view.php
+++ table_view.php
@@ -60,7 +60,7 @@
 		<TABLE class="table table-striped table-condensed" class="maintable" id="maintable">
 	<?php 	$i = 1;
 			while ($project = mysql_fetch_array($projects)) {
-				echo "<TR><TD colspan='15'>".$project[0]."</TD></TR>";
+				echo "<TR><TD colspan='15'><b>".$project[0]."</b></TD></TR>";
 				$sql 	  = "SELECT * FROM `confogen`.`domains` WHERE `project`='".$project[0]."' ORDER BY `ip`;";
     			$result   = mysql_query($sql, $link)  or die(mysql_error());
 				while ($row = mysql_fetch_assoc($result)) { ?>
