f735aab7fc8d4f43717abc540098fe3b2077d5ad
Himaster <himaster@inbox.ru>
2016-08-16 16:47:25 +0300

    db configs for balancer1

--- db.php
+++ db.php
@@ -1,16 +1,9 @@
 <?php
-	if (($_SERVER['HTTP_HOST'] == "confogen1.pkwteile.de") or ($_SERVER['HTTP_HOST'] == "confogen2.pkwteile.de")) $env="master";
-	elseif ($_SERVER['HTTP_HOST'] == "confogen.cdn.pkwteile.de") $env="cdn";
-	elseif ($_SERVER['HTTP_HOST'] == "confogen.loc") $env="dev";
-	else header("Location: https://confogen1.pkwteile.de/");
 
 	$dbuser = 'confogen';
 	$dbpassword = 'Bie0gaen';
 	$dbname = 'confogen';
-
-	if ($env == "master") $dbhost = 'netbox.co';
-	elseif ($env == "cdn") $dbhost = 'cdn';
-	else $dbhost = '127.0.0.1';
+    $dbhost = 'netbox.co';
 
 	#DB
 	$link = mysql_connect($dbhost, $dbuser, $dbpassword);
