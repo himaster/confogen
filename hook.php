4d20a9a7fa33b4cfdc0428d0973dc52cdd7dcef9
Himaster <himaster@inbox.ru>
2016-08-16 16:32:05 +0300

    hook fix

--- hook.php
+++ hook.php
@@ -1,5 +1,5 @@
 <?php
     putenv('PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/var/www/confogen/');
-    exec('cd /var/www/confogen/ && git fetch origin && git reset --hard origin/cdn', $output);
+    exec('cd /var/www/confogen/ && git fetch origin && git reset --hard origin/cdn 2>&1', $output);
     print_r($output);
 ?>
