6774b96434c081dfecd429d5f5b284de1cc63919
Himaster <himaster@inbox.ru>
2016-08-16 16:28:21 +0300

    hook improvement

--- hook.php
+++ hook.php
@@ -1,5 +1,5 @@
 <?php
     putenv('PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/var/www/confogen/');
-    exec('cd /var/www/confogen/ && git pull https://github.com/himaster/confogen.git 2>&1', $output);
+    exec('cd /var/www/confogen/ && git fetch origin && git reset --hard origin/master', $output);
     print_r($output);
 ?>
